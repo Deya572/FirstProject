3643267df6e4495e1d8239dca3b6d6dae111e795
Deya <deya.ppmg@gmail.com>
2026-07-02 10:54:44 +0300

    Add ToFileRow method

--- Person.cs
+++ Person.cs
@@ -23,5 +23,10 @@ namespace FirstProject
         {
             return $"Име: {Name}, Възраст: {Age}, Заплата: {Salary:F2}";
         }
+
+        public string ToFileRow()
+        {
+           return $"{Name};{Age};{Salary}";
+        }
     }
 }
